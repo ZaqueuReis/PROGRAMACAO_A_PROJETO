9d7aab97e91ec7bc0f0658427ea7369fec8cb06c
renan-mendonça-rodrigues <renan.rodrigues@dcomp.ufs.br>
2026-06-19 09:53:04 -0300

    Sexta Versão - Correção do erro de sincronização no github e do erro do default no preenchimento das figuras e linhas invisíveis

--- arquivo.py
+++ arquivo.py
@@ -8,30 +8,30 @@ def iniciar_figura_nova(event):
     global figura_nova
     
     if tipo_figura_var.get() == 'Linha':
-        figura_nova = ("linha", (event.x, event.y, event.x, event.y), cor_borda_var.get()) 
+        figura_nova = ("linha", (event.x, event.y, event.x, event.y), cor_borda_var.get(), cor_preenchimento_var.get()) 
         
     elif tipo_figura_var.get() == 'Oval': # adicionada a figura oval
-        figura_nova = ("oval", (event.x, event.y, event.x, event.y), cor_borda_var.get())
+        figura_nova = ("oval", (event.x, event.y, event.x, event.y), cor_borda_var.get(), cor_preenchimento_var.get())
 
     elif tipo_figura_var.get() == 'Circulo': #adicionada a figura circulo
-        figura_nova = ("circulo", (event.x, event.y, event.x, event.y), cor_borda_var.get())
+        figura_nova = ("circulo", (event.x, event.y, event.x, event.y), cor_borda_var.get(), cor_preenchimento_var.get())
         
     elif tipo_figura_var.get() == 'Retangulo' : #adicionada a figura retangulo
-        figura_nova = ('retangulo', (event.x, event.y, event.x, event.y), cor_borda_var.get())
+        figura_nova = ('retangulo', (event.x, event.y, event.x, event.y), cor_borda_var.get(), cor_preenchimento_var.get())
         
     else :
-        figura_nova = ("rabisco", [(event.x, event.y)], cor_borda_var.get())
+        figura_nova = ("rabisco", [(event.x, event.y)], cor_borda_var.get(), cor_preenchimento_var.get())
 
 # Quando mouse é movido com o botão pressionado
 def atualizar_figura_nova(event):
     
     global figura_nova
     
-    tipo, valores, borda = figura_nova # substituição para evitar o uso de indices em excesso no codigo abaixo
+    tipo, valores, borda, preenchimento = figura_nova # substituição para evitar o uso de indices em excesso no codigo abaixo
 
     if tipo == "rabisco":
         valores.append((event.x, event.y))
-        figura_nova = (tipo, valores, borda)
+        figura_nova = (tipo, valores, borda, preenchimento)
         
     elif tipo == 'circulo': # adicionado forrma para calcular as coordenadas do circulo, considerando deslizamentos do cursor para direita e esquerda
 
@@ -42,10 +42,10 @@ def atualizar_figura_nova(event):
         x2 = x1 + raio if event.x >= x1 else x1 - raio
         y2 = y1 + raio if event.y >= y1 else y1 - raio
 
-        figura_nova = (tipo, (x1, y1, x2, y2), borda)
+        figura_nova = (tipo, (x1, y1, x2, y2), borda, preenchimento)
     
     else:
-        figura_nova = (tipo, (valores[0], valores[1], event.x, event.y), borda)
+        figura_nova = (tipo, (valores[0], valores[1], event.x, event.y), borda, preenchimento)
 
     desenhar_figuras()
     desenhar_figura_nova()
@@ -55,6 +55,9 @@ a função apenas para verificar se é rabisco ou circulo. Se for, já verifica 
 recai no mesmo caso para todas as outras figuras"""
 
 # Quando mouse é solto
+'''Defini uma variavel global para definir o tamnaho da borda padrão para as figuras'''
+tamanho_borda = 2
+
 def incluir_figura_nova(event): 
     if not incompleta(figura_nova): # para evitar incluir figuras incompletas, como uma linha sem comprimento ou um rabisco com um único ponto
         figuras.append(figura_nova) 
@@ -62,33 +65,33 @@ def incluir_figura_nova(event):
 
 def desenhar_figuras():
     canvas.delete("all")
-    for fig, values, borda in figuras:
+    for fig, values, borda, preenchimento in figuras:
         if fig == "linha":
-            canvas.create_line(values[0], values[1], values[2], values[3])
+            canvas.create_line(values[0], values[1], values[2], values[3], fill = borda, width=tamanho_borda) # alterei para fill = borda pois quando iniciava o programa a linha ficava invisivel
         elif fig == "oval": #criado para desenhar ovais passadas
-            canvas.create_oval(values[0], values[1], values[2], values[3], outline = borda)
+            canvas.create_oval(values[0], values[1], values[2], values[3], outline = borda, fill = preenchimento, width=tamanho_borda)
         elif fig == 'retangulo' : #criado para desenhar retangulos passados
-            canvas.create_rectangle(values[0], values[1], values[2], values[3], outline = borda)
+            canvas.create_rectangle(values[0], values[1], values[2], values[3], outline = borda, fill = preenchimento, width=tamanho_borda)
         elif fig == 'circulo' : #criado para desenhar circulos passados
-            canvas.create_oval(values[0], values[1], values[2], values[3], outline = borda)
+            canvas.create_oval(values[0], values[1], values[2], values[3], outline = borda, fill = preenchimento, width=tamanho_borda)
         else : # fig == "rabisco"
-            canvas.create_line(values)
+            canvas.create_line(values, fill = borda, width=tamanho_borda) # alterei para fill = borda pois quando iniciava o programa a linha ficava invisivel
 
 def desenhar_figura_nova():
-    fig, values, borda = figura_nova
+    fig, values, borda, preenchimento = figura_nova
     if fig == "linha":
-        canvas.create_line(values[0], values[1], values[2], values[3], dash=(4, 2))
+        canvas.create_line(values[0], values[1], values[2], values[3], fill = borda, width=tamanho_borda, dash=(4, 2)) # alterei para fill = borda pois quando iniciava o programa a linha ficava invisivel
     elif fig == "oval": #criado para desenhar oval novo
-        canvas.create_oval(values[0], values[1], values[2], values[3], outline = borda, dash =(4, 2))
+        canvas.create_oval(values[0], values[1], values[2], values[3], outline = borda, fill = preenchimento, width=tamanho_borda, dash =(4, 2))
     elif fig == 'circulo': #criado para desenhar retangulo novo
-        canvas.create_oval(values[0], values[1], values[2], values[3], outline = borda, dash=(4,2))
+        canvas.create_oval(values[0], values[1], values[2], values[3], outline = borda, fill = preenchimento, width=tamanho_borda, dash=(4,2))
     elif fig == 'retangulo' : #criado para desenhar circulo novo
-        canvas.create_rectangle(values[0], values[1], values[2], values[3], outline = borda, dash=(4, 2))
+        canvas.create_rectangle(values[0], values[1], values[2], values[3], outline = borda, fill = preenchimento, width=tamanho_borda, dash=(4, 2))
     else : # fig == "rabisco"
-        canvas.create_line(values, dash=(4, 2))
+        canvas.create_line(values, fill = borda, width=tamanho_borda, dash=(4, 2)) # alterei para fill = borda pois quando iniciava o programa a linha ficava invisivel
 
 def incompleta(figura): 
-    fig, values, borda = figura
+    fig, values, borda, preenchimento = figura
     
     if fig == "rabisco":
         return len(values) <= 1
@@ -132,7 +135,7 @@ option_menu.grid(column=1, row=0, sticky=W, **paddings)
 
 # Área de desenho
 canvas = Canvas(frame, bg='white', width=600, height=600)
-canvas.grid(column=0, row=2, columnspan=2, sticky=W, **paddings)
+canvas.grid(column=0, row=3, columnspan=3, sticky=W, **paddings)
 
 frame.pack()
 
@@ -141,13 +144,27 @@ label_2 = ttk.Label(frame, text='Cor da borda:')
 label_2.grid(column=0, row=1, sticky=W, **paddings)
 
 
-# Criação do widgte para selecionar a cor da borda
+# Criação do widgtet para selecionar a cor da borda
 cor_borda_var = StringVar(root)
 
 option_menu_2 = ttk.OptionMenu(frame, cor_borda_var,
-               "black", "black", "red", "green", "blue","orange"
+               "Black", "Black", "Red", "Green", "Blue","Orange"
                 )
 option_menu_2.grid(column=1, row=1, sticky=W, **paddings)
+
+
+# Criação do widget de texto para o preenchimento das figuras
+label_3 = ttk.Label(frame, text = 'Cor Preenchimento')
+label_3.grid(column=0, row=2, sticky=W, **paddings)
+
+
+# Criação do widgtet para selecionar a cor do preenchimento
+cor_preenchimento_var = StringVar(root)
+
+option_menu_3= ttk.OptionMenu(frame, cor_preenchimento_var,
+               "White", "White", "Yellow", "Red", "Green", "Blue","Orange"
+                )
+option_menu_3.grid(column=1, row=2, sticky=W, **paddings)
 
 # Eventos de mouse associados ao canvas - com seus callbacks
 canvas.bind('<ButtonPress-1>', iniciar_figura_nova)
