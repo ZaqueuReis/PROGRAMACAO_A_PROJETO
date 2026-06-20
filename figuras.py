d70c025fab23b4d29c9113f7623fb681bc5e3184
renan-mendonça-rodrigues <renan.rodrigues@dcomp.ufs.br>
2026-06-20 20:07:21 -0300

    OITAVA VERSÃO - IMPLEMENTAÇÃO PARCIAL DO ARQUIVO FIGURAS.PY COM A PROGRAMAÇÃO ORIENTADA A OBJETOS

---
+++ figuras.py
@@ -0,0 +1,96 @@
+from abc import ABC, abstractmethod
+
+class Figura(ABC):
+
+    def __init__(self, cor_borda, cor_preenchimento, tamanho_borda):
+        self.borda = cor_borda
+        self.preenchimento = cor_preenchimento
+        self.tamanho_borda = tamanho_borda
+    
+    @abstractmethod
+    def desenhar(self,canvas):
+        pass
+
+    @abstractmethod
+    def incompleta(self):
+        pass
+
+class Linha(Figura):
+
+    def __init__(self, x1, y1, x2, y2, borda, tamanho_borda):
+        super().__init__(borda, '', tamanho_borda)
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+    
+    def desenhar(self, canvas):
+        canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill = self.borda, width = self.tamanho_borda)
+    
+    def incompleta(self):
+        return self.x1 == self.x2 and self.y1 == self.y2
+
+class Rabisco(Figura):
+    def __init__(self, pontos, borda, tamanho_borda):
+        super().__init__(borda, '', tamanho_borda)
+        self.pontos = pontos
+    
+    def desenhar(self, canvas):
+        canvas.create_line(self.pontos, fill = self.borda, width = self.tamanho_borda)
+    
+    def incompleta(self):
+        return len(self.pontos) <= 1
+
+class Retangulo(Figura):
+    def __init__(self, x1, y1, x2, y2, borda, preenchimento, tamanho_borda):
+        super().__init__(borda, preenchimento, tamanho_borda)
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    def desenhar(self, canvas):
+        canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, outline = self.borda, fill = self.preenchimento, width = self.tamanho_borda)
+    
+    def incompleta(self):
+        return self.x1 == self.x2 and self.y1 == self.y2
+
+class Oval(Figura):
+    def __init__(self, x1, y1, x2, y2, borda, preenchimento, tamanho_borda):
+        super().__init__(borda, preenchimento, tamanho_borda)
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+    
+    def desenhar(self, canvas):
+        canvas.create_oval(self.x1, self.y1, self.x2, self.y2, outline = self.borda, fill = self.preenchimento, width = self.tamanho_borda)
+    
+    def incompleta(self):
+        return self.x1 == self.x2 and self.y1 == self.y2
+
+class Circulo(Figura):
+
+    def __init__(self, centro_x, centro_y, raio, borda, preenchimento, tamanho_borda):
+        super().__init__(borda, preenchimento, tamanho_borda)
+        self.centro_x = centro_x
+        self.centro_y = centro_y
+        self.raio = raio
+
+    def desenhar(self, canvas):
+        canvas.create_oval(self.centro_x - self.raio, self.centro_y - self.raio, self.centro_x + self.raio, self.centro_y + self.raio, outline=self.borda, fill=self.preenchimento, width = self.tamanho_borda)
+
+    def incompleta(self):
+        return self.raio <= 0
+
+class Poligono(Figura):
+
+    def __init__(self, pontos, borda, preenchimento, tamanho_borda):
+        super().__init__(borda, preenchimento, tamanho_borda)
+        self.pontos = pontos
+
+    def desenhar(self, canvas):
+        canvas.create_polygon(self.pontos, outline=self.borda, fill=self.preenchimento, width = self.tamanho_borda)
+
+    def incompleta(self):
+        return len(self.pontos) < 3
