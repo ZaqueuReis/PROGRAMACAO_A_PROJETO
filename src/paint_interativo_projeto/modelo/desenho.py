a4e38d0122f48689c88f67b318ac5138bd26d6bc
renan-mendonça-rodrigues <renan.rodrigues@dcomp.ufs.br>
2026-06-30 10:34:42 -0300

    VERSÃO 13.1 -- AJUSTE

--- src/paint_interativo_projeto/modelo/desenho.py
+++ src/paint_interativo_projeto/modelo/desenho.py
@@ -3,21 +3,25 @@ class Desenho :
         self.figuras = []
         self.figura_atual = None
 
+    # ========= Para figuras em construção no momento do desenho ==========
+
+    def inicializar_figura_atual(self, figura) :
+        self.figura_atual = figura
+    
+    def obter_figura_atual(self) :
+        return self.figura_atual
+    
+    # ========= Para as figuras que estão na lista self.figuras ===========
+
     def adicionar_figura_concluida(self) :
         if self.figura_atual :
             self.figuras.append(self.figura_atual)
             self.figura_atual = None
 
-    def inicializar_figura_atual(self, figura) :
-        self.figura_atual = figura
-
-
-    #Apartir desta função obter o controller irá ficar a par da lista de figuras
+    # A partir desta função obter o controller irá ficar a par da lista de figuras
     def obter_figuras(self) :
         return self.figuras
 
-    def obter_figura_atual(self) :
-        return self.figura_atual
     
     '''Sinceramente, acredito eu que, usar o padrão MVC, ajuda não so nós que estamos
     desenvolvendo, a enteder precisamente o código, como também outras pessoas que
